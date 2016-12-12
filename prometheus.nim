297a5752a2d64edc96d38e9f9542f031a57e6c1f
Alex Chistyakov <alexclear@gmail.com>
2016-12-12 08:31:50 +0300

    Store counters and export stored metrics

--- prometheus.nim
+++ prometheus.nim
@@ -1,13 +1,38 @@
-import tables
+import tables, lists, locks, strutils
 
 type
-  Counter* = object of RootObj
+  StatItem = ref object of RootObj
+
+  Counter* = ref object of StatItem
     name: string
     help: string
     value: int
 
+var
+  statLock: Lock
+  statList: SinglyLinkedList[StatItem]
+
+method exportMetrics(item: StatItem): string {.base.} =
+  result = ""
+
+method exportMetrics(item: Counter): string =
+  result = ""
+  result = result & "# HELP " & ((Counter)(item)).name & " " & ((Counter)item).help & "\n"
+  result = result & "# TYPE " & ((Counter)(item)).name & " counter\n"
+  result = result & ((Counter)(item)).name & " " & formatFloat(toFloat(item.value), ffDecimal, 1)
+
 proc newCounter*(name: string, help: string): Counter =
   result = Counter(name: name, help: help, value: 0)
+  statLock.acquire()
+  statList.prepend(result)
+  statLock.release()
 
 proc increment*(obj: var Counter) =
   atomicInc(obj.value)
+
+proc exportAllMetrics*(): string =
+  result = ""
+  for statItem in items(statList):
+    result = result & statItem.exportMetrics()
+
+initLock(statLock)
