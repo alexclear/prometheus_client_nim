2e51224f6278d183e8128258194fb3cec8cd9227
Alex Chistyakov <alexclear@gmail.com>
2016-12-12 15:28:22 +0300

    Eliminate global vars

--- prometheus.nim
+++ prometheus.nim
@@ -3,19 +3,19 @@ import tables, lists, locks, strutils
 type
   StatItem = ref object of RootObj
 
-  Counter* = ref object of StatItem
+  Counter = ref object of StatItem
     name: string
     help: string
     value: int
 
-  Histogram* = ref object of StatItem
+  Histogram = ref object of StatItem
     name: string
     help: string
     bucketMargins: seq[float]
 
-var
-  statLock: Lock
-  statList: SinglyLinkedList[StatItem]
+  Prometheus = ref object of RootObj
+    statLock: Lock
+    statList: SinglyLinkedList[StatItem]
 
 method exportMetrics(item: StatItem): string {.base.} =
   result = ""
@@ -26,20 +26,24 @@ method exportMetrics(item: Counter): string =
   result = result & "# TYPE " & item.name & " counter\n"
   result = result & item.name & " " & formatFloat(toFloat(atomicInc(item.value, 0)), ffDecimal, 1) & "\n"
 
-proc newCounter*(name: string, help: string): Counter =
+proc newPrometheus*(): Prometheus =
+  result = Prometheus()
+  initLock(result.statLock)
+
+proc newCounter*(obj: Prometheus, name: string, help: string): Counter =
   result = Counter(name: name, help: help, value: 0)
-  statLock.acquire()
-  statList.prepend(result)
-  statLock.release()
+  obj.statLock.acquire()
+  obj.statList.prepend(result)
+  obj.statLock.release()
 
 proc increment*(obj: var Counter) =
   atomicInc(obj.value)
 
-proc newHistogram*(name: string, help: string, bucketMargins: openArray[float]): Histogram =
+proc newHistogram*(obj: Prometheus, name: string, help: string, bucketMargins: openArray[float]): Histogram =
   result = Histogram(name: name, help: help, bucketMargins: @bucketMargins)
-  statLock.acquire()
-  statList.prepend(result)
-  statLock.release()
+  obj.statLock.acquire()
+  obj.statList.prepend(result)
+  obj.statLock.release()
 
 proc observe*(obj: var Histogram, value: float) =
   echo "Observing...\n"
@@ -49,9 +53,9 @@ method exportMetrics(item: Histogram): string =
   result = result & "# HELP " & item.name & " " & item.help & "\n"
   result = result & "# TYPE " & item.name & " histogram\n"
 
-proc exportAllMetrics*(): string =
+proc exportAllMetrics*(obj: Prometheus): string =
   result = ""
-  for statItem in items(statList):
+  obj.statLock.acquire()
+  for statItem in items(obj.statList):
     result = result & statItem.exportMetrics()
-
-initLock(statLock)
+  obj.statLock.release()
