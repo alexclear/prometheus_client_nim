80e1c23796533fa6ab0f73ffbffaaa2d97ead88f
Alex Chistyakov <alexclear@gmail.com>
2016-12-12 14:41:23 +0300

    Start adding histogram

--- prometheus.nim
+++ prometheus.nim
@@ -8,6 +8,11 @@ type
     help: string
     value: int
 
+  Histogram* = ref object of StatItem
+    name: string
+    help: string
+    bucketMargins: seq[float]
+
 var
   statLock: Lock
   statList: SinglyLinkedList[StatItem]
@@ -17,9 +22,9 @@ method exportMetrics(item: StatItem): string {.base.} =
 
 method exportMetrics(item: Counter): string =
   result = ""
-  result = result & "# HELP " & ((Counter)(item)).name & " " & ((Counter)item).help & "\n"
-  result = result & "# TYPE " & ((Counter)(item)).name & " counter\n"
-  result = result & ((Counter)(item)).name & " " & formatFloat(toFloat(item.value), ffDecimal, 1)
+  result = result & "# HELP " & item.name & " " & item.help & "\n"
+  result = result & "# TYPE " & item.name & " counter\n"
+  result = result & item.name & " " & formatFloat(toFloat(atomicInc(item.value, 0)), ffDecimal, 1) & "\n"
 
 proc newCounter*(name: string, help: string): Counter =
   result = Counter(name: name, help: help, value: 0)
@@ -29,6 +34,20 @@ proc newCounter*(name: string, help: string): Counter =
 
 proc increment*(obj: var Counter) =
   atomicInc(obj.value)
+
+proc newHistogram*(name: string, help: string, bucketMargins: openArray[float]): Histogram =
+  result = Histogram(name: name, help: help, bucketMargins: @bucketMargins)
+  statLock.acquire()
+  statList.prepend(result)
+  statLock.release()
+
+proc observe*(obj: var Histogram, value: float) =
+  echo "Observing...\n"
+
+method exportMetrics(item: Histogram): string =
+  result = ""
+  result = result & "# HELP " & item.name & " " & item.help & "\n"
+  result = result & "# TYPE " & item.name & " histogram\n"
 
 proc exportAllMetrics*(): string =
   result = ""
